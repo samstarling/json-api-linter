ed04bfad836b9a650d51e278ad1ae0c5cee1c6fc
Sam Starling <sam@soundcloud.com>
2015-04-16 21:48:08 +0200

    Print the body of an HTTP response

--- hello.go
+++ hello.go
@@ -1,7 +1,20 @@
 package main
 
 import "fmt"
+import "net/http"
+import "io/ioutil"
 
+func perror(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
 func main() {
-	fmt.Printf("Hello, world.\n")
+    url := "http://samstarling.co.uk"
+    res, err := http.Get(url)
+    perror(err)
+    defer res.Body.Close()
+    body, err := ioutil.ReadAll(res.Body)
+    perror(err)
+    fmt.Printf("%s", body)
 }
