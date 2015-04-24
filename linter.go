1ead6330735f57a9b6583f491285ea2155063592
Sam Starling <sam@soundcloud.com>
2015-04-24 16:56:29 +0100

    Split into functions

--- linter.go
+++ linter.go
@@ -11,16 +11,30 @@ func main() {
 	args := os.Args[1:]
 
 	url := args[0]
+	res := fetch(url)
+	dat := parse(res)
+	validate(dat)
+}
+
+func fetch(url string) *http.Response {
 	res, err := http.Get(url)
 	perror(err)
-	defer res.Body.Close()
+	return res
+}
+
+func parse(res *http.Response) map[string]interface{} {
 	body, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
 	perror(err)
 
 	var dat map[string]interface{}
 	if err := json.Unmarshal(body, &dat); err != nil {
 		panic(err)
 	}
+	return dat
+}
+
+func validate(dat map[string]interface{}) {
 	if _, ok := dat["name"]; ok {
 		fmt.Println("Name element found")
 	} else {
